074347121dc126e672c3075c6819f6f4cb8d38c6
meili <820855278@qq.com>
2021-08-17 00:25:20 +0800

    ‘登录界面处理

--- src/main/java/com/xbky/circuitManger/Main.java
+++ src/main/java/com/xbky/circuitManger/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../../../sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("../../../index.fxml"));
+        primaryStage.setTitle("产品维修管理软件");
+        primaryStage.setScene(new Scene(root, 1600, 800));
         primaryStage.show();
     }
 
