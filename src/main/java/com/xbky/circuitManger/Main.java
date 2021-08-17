67d529f45182fefe37c0b70e9d85c22060b26b23
meili <820855278@qq.com>
2021-08-18 00:14:57 +0800

    ‘保存

--- src/main/java/com/xbky/circuitManger/Main.java
+++ src/main/java/com/xbky/circuitManger/Main.java
@@ -4,15 +4,19 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../../../index.fxml"));
-        primaryStage.setTitle("产品维修管理软件");
-        primaryStage.setScene(new Scene(root, 1600, 800));
+        primaryStage.getIcons().add(new Image("/assets/imgs/logo.png")); // 页面logo
+        primaryStage.setTitle("产品维修管理软件"); // 页面标题
+        Parent root = FXMLLoader.load(getClass().getResource("/index.fxml")); // 页面对应的fxml
+        Scene primarySence = new Scene(root, 1600, 800); // 页面大小
+        primaryStage.setScene( primarySence );
+        primarySence.getStylesheets().add(getClass().getResource("/css/login.css").toExternalForm());// 页面样式表
         primaryStage.show();
     }
 
--- src/main/java/com/xbky/circuitManger/controller/Controller.java
+++ src/main/java/com/xbky/circuitManger/controller/Controller.java
@@ -1,4 +1,12 @@
 package com.xbky.circuitManger.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 public class Controller {
+    public Label username;
+    public void handleClick(ActionEvent actionEvent) {
+//        username.setText( "12321" );
+    };
 }
