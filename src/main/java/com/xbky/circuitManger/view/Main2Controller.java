7afe0d501c1e90664d61057f1e60ef3065fe24df
004485 <zhangzhuolun@fxbox.com>
2021-08-18 12:37:12 +0800

    更新修改

--- src/main/java/com/xbky/circuitManger/view/Main2Controller.java
+++ src/main/java/com/xbky/circuitManger/view/Main2Controller.java
@@ -1,11 +1,14 @@
 package com.xbky.circuitManger.view;
 
 import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class Controller implements Initializable {
+
+public class Main2Controller implements Initializable {
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
