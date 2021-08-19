3829336935da95c69fd14adee23c6cd79c5c9267
zhangzhuol@133.cn <zhangzhuol@133.cn>
2021-08-20 01:51:27 +0800

    菜单样式调节

--- src/main/java/com/xbky/circuitManger/view/fx/FxmlView.java
+++ src/main/java/com/xbky/circuitManger/view/fx/FxmlView.java
@@ -7,7 +7,7 @@ public enum FxmlView {
         @Override
         public String title() {return "初始页";}
         @Override
-        public String fxml() {return "/views/main.fxml";}
+        public String fxml() {return "/views/main2.fxml";}
     },
     BASESET_PT{
         @Override
