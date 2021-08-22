e6e3ff20464c0db3cc61638fcaff17fc75424171
zhangzhuol@133.cn <zhangzhuol@133.cn>
2021-08-22 19:52:49 +0800

    更改

--- src/main/java/com/xbky/circuitManger/Main.java
+++ src/main/java/com/xbky/circuitManger/Main.java
@@ -5,6 +5,10 @@ import com.xbky.circuitManger.view.common.FxmlView;
 import com.xbky.circuitManger.view.common.StageManager;
 import com.xbky.circuitManger.view.common.StageManagerSingleton;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -13,7 +17,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
 //        primaryStage.getIcons().add(new Image("/assets/imgs/logo.png")); // 页面logo
 //        primaryStage.setTitle("产品维修管理软件"); // 页面标题
-//        Parent root = FXMLLoader.load(getClass().getResource("/views/main.fxml")); // 页面对应的fxml
+//        Parent root = FXMLLoader.load(getClass().getResource("/views/baseSet_info.fxml")); // 页面对应的fxml
 //        Scene primarySence = new Scene(root, 1600, 800); // 页面大小
 //        primaryStage.setScene( primarySence );
 //        primarySence.getStylesheets().add(getClass().getResource("/css/login.css").toExternalForm());// 页面样式表
