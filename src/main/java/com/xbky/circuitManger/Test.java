403cfdf1883090eee5e1e90ca08ebb7995e84ea8
zhangzhuol@133.cn <zhangzhuol@133.cn>
2021-09-04 16:40:01 +0800

    Excel 测试

--- src/main/java/com/xbky/circuitManger/Test.java
+++ src/main/java/com/xbky/circuitManger/Test.java
@@ -1,8 +1,12 @@
 package com.xbky.circuitManger;
 
+import com.xbky.circuitManger.utils.ExcelUtil;
+
+import java.io.IOException;
+
 public class Test{
     
-    public static void main(String[] args) {
-        System.out.println("hello world");
+    public static void main(String[] args) throws IOException {
+        ExcelUtil.sheet();
     }
 }
